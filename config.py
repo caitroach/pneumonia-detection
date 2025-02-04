1d56a804a2cff1f85fe44b591f8db13328c40d7c
Cait Roach <155276726+CaitlinRoach06@users.noreply.github.com>
2025-02-04 12:09:01 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 '''
-I'm gonna trust your powers of common sense here: 
-I won't tell you directly to change this path to your own dataset path, so you need to figure this step out on your own.
-You can do this. I believe in you. 
+This project WILL NOT WORK ON YOUR DEVICE unless you change the dataset path below to your own!
 '''
 
-YOUR_DATASET_PATH = r"C:\Users\Roach\Desktop\pneumonia_project\chest_xray" #Might wanna alter this. Not sure. You tell me...
+YOUR_DATASET_PATH = r"C:\Users\Roach\Desktop\pneumonia_project\chest_xray" 
